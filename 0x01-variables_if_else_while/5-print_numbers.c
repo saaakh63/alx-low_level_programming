ca1c48f600256d35246ae53d8343b8fc069a7633
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-09 02:25:37 +0000

    dd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	srand(time(0));
+	while (i <= 9)
+	{
+		printf("%i", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
