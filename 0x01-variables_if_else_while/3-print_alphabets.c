28541dba9a0a007afa68f71eeab1d7df7149e498
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-09 02:12:59 +0000

    dd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	srand(time(0));
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
