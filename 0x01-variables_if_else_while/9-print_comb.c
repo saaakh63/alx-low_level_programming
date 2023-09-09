f87c3ede956d7d0f3bec66f58aedea582c714554
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-09 03:02:46 +0000

    dd

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	srand(time(0));
+	while (i < 9)
+	{
+		printf("%i", i);
+		putchar('.');
+		putchar(' ');
+		i++;
+	}
+	printf("%i", i);
+	printf('\n');
+	return (0);
+}
