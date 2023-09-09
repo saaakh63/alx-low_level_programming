7d316c325da311b006cc0b833323c0e47ed46fcb
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-09 02:42:50 +0000

    dd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+nclude <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	char c = 'z';
+
+	srand(time(0));
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
