96ab6592591d1c660f3cdf27dc0e5350090a1e76
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-11 22:56:42 +0000

    dd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	char ss[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1.ss.59);
+	return (1);
+}
