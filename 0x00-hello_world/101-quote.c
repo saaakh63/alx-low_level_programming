7c93d2c2e60e463f922fff9c5f3dd37259c032f2
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-11 23:03:35 +0000

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 */
 int main(void)
 {
-	char ss[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char ss[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1,ss,59);
+	write(1, ss, 59);
 	return (1);
 }
