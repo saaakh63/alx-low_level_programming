b45ce38c4a7666268bdd81b1d24cbf7f23ddff74
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-11 22:11:15 +0000

    ff

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
