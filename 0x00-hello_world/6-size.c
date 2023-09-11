c34511723231c55130291b25362c6b6ec79bbf19
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-11 22:33:53 +0000

    ddd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <dtdio.h>
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
