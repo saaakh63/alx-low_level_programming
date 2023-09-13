0657b2bbb5850069de6f43ddbd2de2d64e3e44c9
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-13 23:30:26 +0000

    gg

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* jack_bauer - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int jack_bauer(void)
+{
+	int i, j, k, z;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (z = 0; z <= 9; z++)
+				{
+					_putchar((i) + 48);
+					_putchar((j) + 48);
+					_putchar(':');
+					_putchar((k) + 48);
+					_putchar((z) + 48);
+					if (i == 2 && j == 3 && k == 5 && z == 9)
+					{           
+						return (0);
+					}                   
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
