6b8fda4a5fe0941a01a2348a0e73909a64df6f81
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-13 02:01:22 +0000

    ff

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+* main - entry point
+*
+* Description: false or true validation
+*
+*Return: 0 (success)
+*/
+int main(void)
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+
+	return (0);
+}
