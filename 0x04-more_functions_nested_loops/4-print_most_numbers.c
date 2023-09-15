280e78683ca1fe94c29b6f833b810140f464bedd
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-15 01:20:46 +0000

    kk

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_most_numbers - entry point
+*
+* @: false or true validation
+*
+*Return: 0 (success)
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(48 + i);
+	}
+	-putchar('\n');
+}
