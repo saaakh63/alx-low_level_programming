2a32f7a3fac6b5d0f70180c107edc4ec88504cd0
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-15 02:40:16 +0000

    hh

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_triangle - entry point
+*
+* @c: false or true validation
+*
+*Return: 0 (success)
+*/
+void print_triangle(int c)
+{
+	int i, j;
+	if (c <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < c; i++)
+		{
+			for (j = 0; j < c; j++)
+			{
+				if (i + j <= c)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
