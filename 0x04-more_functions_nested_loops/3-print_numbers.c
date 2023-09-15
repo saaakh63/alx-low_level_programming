491dcf1d073bf664fce8489ccce78c5da2f9adcf
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-15 01:04:30 +0000

    ff

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - entry point
+*
+* @: false or true validation
+*
+*Return: 0 (success)
+*/
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(48 + i)
+	}
+}
