8bfa9d1b7e708e80cc7c607086eccb2425f631d0
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-15 01:38:05 +0000

    ff

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* more_numbers - entry point
+*
+* @: false or true validation
+*
+*Return: 0 (success)
+*/
+void more_numbers(void)
+{
+	int i, j, zz;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			zz = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				zz = j % 10;
+			}
+			_putchar(zz + 48);
+		}
+	}
+}
