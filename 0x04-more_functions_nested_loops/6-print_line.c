f13470f74f73700e331c8c05976713553f399507
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-15 01:46:01 +0000

    ff

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_line - entry point
+*
+* @n: false or true validation
+*
+*Return: 0 (success)
+*/
+void print_line(int n)
+{
+	while (n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
