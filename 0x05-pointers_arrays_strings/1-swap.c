30806610b43c40f10a2dca67f9f3ce35d7c810c1
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-18 23:49:59 +0000

    ff

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* swap_int - entry point
+*
+* @a: false or true validation
+* @b: fffff
+*Return: 0 (success)
+*/
+void swap_int(int *a, int *b)
+{
+	int p;
+
+	p = *a;
+	*a = *b;
+	*b = &p;
+	*b = p;
+}
