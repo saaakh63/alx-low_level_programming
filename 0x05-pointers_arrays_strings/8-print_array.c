099b4ce65f38dbd41cff7d8405ca78f0bf5fd0e7
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-19 02:01:23 +0000

    ff

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+/**
+* print_array - entry point
+*
+* @c: false or true validation
+* @n: ggggg
+*Return: 0 (success)
+*/
+void print_array(int *c, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(c[i] + '0');
+	_putchar('\n');
+}
