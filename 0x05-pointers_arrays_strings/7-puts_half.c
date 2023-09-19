f1c455d5745f54d2dad8a184764f69eaeb2864ee
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-19 01:44:00 +0000

    ff

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* puts_half - entry point
+*
+* @c: false or true validation
+*
+*Return: 0 (success)
+*/
+void puts_half(char *c)
+{
+	int i = 0, j;
+
+	while (c[i])
+		i++;
+	for (j = (i - 1) / 2; j < i; j++)
+		_putchar(c[j]);
+	_putchar('\n');
+}
