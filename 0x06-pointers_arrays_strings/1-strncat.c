9a17abbdbd494c6364b87f85d369c6cc8496b438
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-21 23:32:56 +0000

    gg

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,21 @@
 #include "main.h"
 /**
-* *_strncat - entry point
+* *_strcat - entry point
 *
 * @dest: false or true validation
-* @src: ffff
-* @n: fdfsf
+* @src: gggg
+* @n: gggg
 *Return: 0 (success)
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
+	int i, j;
 
+	i = 0;
+	while (dest[i])
+		i++;
+	for (j = 0; i < n && src[j] != '\0'; j++)
+		dest[i++] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
 }
