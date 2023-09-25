18b43b2c2f2de0c3e6378e84b11af3448695e5b4
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-25 23:53:43 +0000

    ff

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * _strchr - entry point
+*
 * @s: vvv
 * @c: false or true validation
 *
@@ -9,11 +10,12 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+
 	while (s[i])
 	{
 		if (s[i] == c)
 			return (s + i);
 		i++;
 	}
-	return ('\0');	
+	return ('\0');
 }
