005820d9a24d4f7bd46fe7e5d21f5e3f10b80f7d
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-25 23:50:04 +0000

    ff

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strchr - entry point
+* @s: vvv
+* @c: false or true validation
+*
+*Return: 0 (success)
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s[i]);
+		i++;
+	}
+	return ('\0');	
+}
