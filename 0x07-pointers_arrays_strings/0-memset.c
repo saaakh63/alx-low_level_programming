537ad71cd2bb50c141604d17f9fee793ee31861d
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-25 22:38:33 +0000

    gg

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _memset - entry point
+*
+* @s: false or true validation
+* @b: ddddddd
+* @n: fffffffff
+*Return: 0 (success)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x = 0;
+
+	while (n--)
+	{
+		s[x] = b;
+		x++;
+	}
+	return (s);
+}
