c66fefa070bf77522f1fcd1e6b83fed8eec1751c
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-26 00:44:01 +0000

    Ff

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strpbrk - entry point
+*
+* @c: false or true validation
+* @s: gggg
+*Return: 0 (success)
+*/
+char *_strpbrk(char *s, char *c)
+{
+	int i = 0, j = 0;
+
+	while (s[i])
+	{
+		j = 0;
+		while (c[i])
+		{
+			if (c[j] == s[i])
+				return (s[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
