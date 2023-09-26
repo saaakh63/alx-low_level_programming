b3cb435d67c92cef2f7e72a93104ddfdec93c40d
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-09-26 01:05:07 +0000

    ff

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* _strstr - entry point
+*
+* @c: false or true validation
+* @s: ssss
+*Return: 0 (success)
+*/
+char *_strstr(char *s, char *c)
+{
+
+	int i = 0, j = 0, n = 0, z = 0;
+	char *x;
+
+	while (c[j)
+		j++;
+	while (s[i])
+	{
+		n = 0;
+		while (c[n])
+		{
+			if (c[n] != s[i])
+			{
+				break;
+			}
+			z++;
+			n++;
+		}
+		if (z == j)
+			return (c);
+		i++;
+	}
+	return (0);
+}
