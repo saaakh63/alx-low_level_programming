969fb8b4dde113ca8905850c8684b71188840069
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-01 13:12:47 -0700

    gg

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,22 @@
-nclude "main.h"
+#include "main.h"
 /**
 * is_prime_number - entry point
 *
 * @c: false or true validation
-*
+* @y: ddddd
 *Return: 0 (success)
 */
+int test(int c, int y);
 int is_prime_number(int c)
 {
-
+	return (test(c, 2));
+}
+int test(int c, int y)
+{
+	if (y >= c && c > 1)
+		return (1);
+	else if (c % y == 0 || c <= 0)
+	       return (0);
+	else 
+		return (test(c, y + 1));	
 }
