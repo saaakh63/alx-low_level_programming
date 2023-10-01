629744db7ae19617a5fa396c8fd0f8f309ed787b
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-01 13:18:30 -0700

    gg

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,12 +11,19 @@ int is_prime_number(int c)
 {
 	return (test(c, 2));
 }
+/**
+* test - entry point
+*
+* @c: false or true validation
+* @y: ddddd
+*Return: 0 (success)
+*/
 int test(int c, int y)
 {
 	if (y >= c && c > 1)
 		return (1);
-	else if (c % y == 0 || c <= 0)
-	       return (0);
-	else 
-		return (test(c, y + 1));	
+	else if (c % y == 0 || c <= 1)
+		return (0);
+	else
+		return (test(c, y + 1));
 }
