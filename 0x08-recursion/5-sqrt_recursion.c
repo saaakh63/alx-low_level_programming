206c3080e0b9db4d415fb6e2ffbda15c8ae64e5e
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-01 12:58:23 -0700

    gg

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,8 +7,24 @@
 *
 *Return: 0 (success)
 */
+int s(int n, int v);
 int _sqrt_recursion(int n)
 {
-	double x = n;
-	return (sqrt(x));
+        return (s(n, 1));
+}
+/**
+* s - entry point
+*
+* @n: false or true validation
+* @v: ssssss
+*Return: 0 (success)
+*/
+int s(int n, int v)
+{
+        if (v * v == n)
+                return (n);
+        else if (v *v < n)
+                return (s(n, v + 1));
+        else
+                return (-1);
 }
