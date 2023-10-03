0c718fd9527ac86e252b627a014c557877024313
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-02 19:30:34 -0700

    ff

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* @argc: false or true validation
+* @argv: ggg
+*Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int mul;
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf(%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
