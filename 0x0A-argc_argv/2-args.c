534fa4a42fe9a0d937ea050fb62bdc6ae7b72eee
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-02 19:18:25 -0700

    gg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* @argc: false or true validation
+* @argv: ggg
+*Return: 0 (success)
+*/
+int main(int argc, char const *argv[])
+{
+	int w = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
