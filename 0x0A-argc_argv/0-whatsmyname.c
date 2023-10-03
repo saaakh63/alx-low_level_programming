549023d0b57131338f7878ea1ad63bc3f0cae537
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-02 19:57:51 -0700

    gg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
 * @argv: ggg
 *Return: 0 (success)
 */
-int main(int argc __attribute__((unused)), char const *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
