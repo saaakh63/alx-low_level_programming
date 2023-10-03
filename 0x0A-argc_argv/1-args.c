979accae7bbbb1bea453be1bf0da150d21a4ffed
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-02 19:06:09 -0700

    ff

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* @argc: false or true validation
+* @argv: ggg
+*Return: 0 (success)
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
