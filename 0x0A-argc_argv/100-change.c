19ec62e91bbb756e9d1b04ec72572d61487b6aeb
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-02 18:28:36 -0700

    gg

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main-prints - entry point
+*
+* @argc: false or true validation
+* @argv: ggg
+*Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, l = 0, m = atoi(argv[1]);
+		int arr[] = {1, 2, 5, 10, 25};
+		for (i = 0; i < 5; i++)
+		{
+			if (m > arr[i])
+			{
+				l += m / arr[i];
+				m = m % arr[i];
+				if (m % arr[i] == 0)
+					break;
+			}
+		}
+		printf("%d\n", l);
+	}
+	else 
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
