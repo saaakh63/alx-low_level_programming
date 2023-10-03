9f733e198307fca417fb0f0fcdd6876ada749064
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-02 17:51:49 -0700

    gg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - entry point
+*
+* @argc: false or true validation
+* @argv: ggg
+*Return: 0 (success)
+*/
+void main(int argc, char conset *argv[])
+{
+	(void)argc;
+	printf("%s\n",argv[0]);
+}
