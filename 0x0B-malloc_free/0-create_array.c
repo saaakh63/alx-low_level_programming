0803d5b5d98999371fe3fa945d7484a5d0f9b2a6
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-05 18:21:52 -0700

    gg

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* create_array - entry point
+*
+* @c: false or true validation
+* @size: ffff
+*Return: 0 (success)
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size);
+
+	if (size == 0 || m == NULL)
+		return (0);
+	while (size--)
+		ptr[size] = c;
+	return (n);
+}
+
