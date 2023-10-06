7f3de4ccf6937635436d9c00067e156825b36483
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-05 19:22:05 -0700

    gg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,5 +10,38 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	
+	char *ch;
+	int i = 0, j = 0, k = 0;
+
+	if (s1 != NULL)
+	{
+		while (s1[i])
+		{
+			i++;
+		}
+	}
+	if (s2 != NULL)
+	{
+		while (s2[j])
+		{
+			j++;
+		}
+	}
+	ch = malloc((i + j + 1) * sizeof(char));
+	if (ch == NULL)
+	{
+		return (NULL);
+	}
+	while (k < i)
+	{
+		ch[k] = s1[k];
+		k++;
+	}
+	while (k < i + j)
+	{
+		ch[k] = s2[k - i];
+		k++;
+	}
+	ch[k + 1] = '\0';
+	return (ch);
 }
