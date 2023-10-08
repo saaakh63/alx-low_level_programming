27f8d1af0c8f42cacb4ec59070dd14673cd260b8
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-08 16:28:29 -0700

    gg

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _realloc - entry point
+*
+* @ptr: false or true validation
+* @new: ffff
+* @old: ddddddddddd
+*Return: 0 (success)
+*/
+void *_realloc(void *ptr, unsigned int old, unsigned int new)
+{
+	if (new == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+		ptr = malloc(nw);
+
+	if (new == old)
+		return (ptr);
+
+	free(ptr);
+	ptr = malloc(new);
+
+	return (ptr);
+}
