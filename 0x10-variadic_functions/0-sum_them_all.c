fd7baf5c91af80fc2a116caac87988bc4e51ddf1
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-12 17:00:25 -0700

    ggg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @c: fffffffff 
+ *
+ * Return: sum of its parameters.
+ */
+int sum_them_all(const unsigned int c, ...)
+{
+	va_list valist;
+	unsigned int i;
+	int s = 0;
+
+	if (c == 0)
+		return (0);
+
+	va_start(valist, c);
+
+	for (i = 0; i < c; i++)
+		s += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (s);
+}
