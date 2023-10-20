77ecfacbb7550987025862217b52b704739e8b0d
sara <sara.Khalefa708@compit.aun.edu.eg>
2023-10-19 17:49:56 -0700

    gg

---
+++ 0x12-singly_linked_lists/00-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* rint_list - entry point
+*
+* @h: false or true validation
+*
+*Return: 0 (success)
+*/
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
